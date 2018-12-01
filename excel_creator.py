d306dcdd3c092598c117ab324b62c058d500ccd4
prudkovskiy <prudkovskiy@group-ib.ru>
2018-12-01 12:30:46 +0300

    done

---
+++ excel_creator.py
@@ -0,0 +1,262 @@
+# -*- coding: utf-8 -*-
+"""
+asvt_fingerprint.excel
+
+Created by prudkovskiy on 29.11.18 23:51
+"""
+from datetime import datetime, timedelta
+import pandas as pd
+import xlrd
+import xlsxwriter
+import calendar
+from time import sleep
+import re
+
+__author__ = 'prudkovskiy'
+
+week = {
+    0: 'Пн',
+    1: 'Вт',
+    2: 'Ср',
+    3: 'Чт',
+    4: 'Пт',
+    5: 'Сб',
+    6: 'Вс'
+}
+alph = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+file_name = 'asvt.xlsx'
+
+
+def make_start_excel(filename):
+    """
+    Создание шаблона таблицы
+    :param filename:
+    :return:
+    """
+    global week, alph
+
+    sheet_name = datetime.now().strftime("%Y.%m")
+    year_month = sheet_name.split('.')
+    number_of_days = calendar.monthrange(int(year_month[0]), int(year_month[1]))[1]
+    last_column_name = 'A' + alph[(number_of_days + 5) % 27]
+
+    # Создаем новый excel файл и новый рабочий лист, где имя - это год и месяц
+    writer = pd.ExcelWriter(filename, engine='xlsxwriter')
+    workbook = writer.book
+    worksheet = workbook.add_worksheet(sheet_name)  # worksheet name 2018.11
+
+    # Add a bold format to use to highlight cells.
+    bold = workbook.add_format({'bold': True})
+    # Widen the first column to make the text clearer.
+    worksheet.set_column('A:A', 10)
+    worksheet.set_column('B:B', 20, bold)
+    worksheet.set_column('C:E', 12)
+    worksheet.set_column('F:{}'.format(last_column_name), 6)
+
+    worksheet.write(0, 0, 'Номер')
+    worksheet.write(0, 1, 'Фамилия')
+    worksheet.write(0, 2, 'Должность')
+    worksheet.write(0, 3, 'На работе')
+    worksheet.write(0, 4, 'Время входа')
+
+    for i in range(number_of_days):
+        day_of_month = i + 1
+        day_of_week = week[datetime.strptime('{}.{}'.format(sheet_name, day_of_month), '%Y.%m.%d').weekday()]
+        worksheet.write(0, i + 5, '{}|{}'.format(day_of_week, day_of_month))
+
+    worksheet.write(0, 5 + number_of_days, 'Суммарно за месяц')
+
+    # worksheet.write(1, 0, 1)
+    # worksheet.write(1, 1, 'Рафиков А.Г.')
+    # worksheet.write(1, 2, 'Ведущий инженер')
+    # worksheet.write(1, 3, 'Да')
+    # worksheet.conditional_format('A1:B3'.format(last_column_name), {'type': '3_color_scale'})
+
+    writer.save()
+
+
+def create_new_sheet(filename):
+    """
+    Создает новый лист в excel для отчета в новый месяц
+    :param filename:
+    :return:
+    """
+    new_sheet_name = datetime.now().strftime("%Y.%m")
+    year_month = new_sheet_name.split('.')
+    number_of_days = calendar.monthrange(int(year_month[0]), int(year_month[1]))[1]
+
+    # open the file for reading
+    wbRD = xlrd.open_workbook(filename)
+    sheets = wbRD.sheets()
+
+    # open the same file for writing (just don't write yet)
+    wb = xlsxwriter.Workbook(filename)
+
+    # run through the sheets and store sheets in workbook
+    # this still doesn't write to the file yet
+    for sheet in sheets:  # write data from old file
+        same_sheet = wb.add_worksheet(sheet.name)
+        for row in range(sheet.nrows):
+            for col in range(sheet.ncols):
+                same_sheet.write(row, col, sheet.cell(row, col).value)
+
+    new_sheet = wb.add_worksheet(new_sheet_name)
+
+    prev_sheet = sheets.pop()
+    for row in range(prev_sheet.nrows):
+        for col in range(5):
+            new_sheet.write(row, col, prev_sheet.cell(row, col).value)
+
+    for i in range(number_of_days):
+        day_of_month = i + 1
+        day_of_week = week[datetime.strptime('{}.{}'.format(new_sheet_name, day_of_month), '%Y.%m.%d').weekday()]
+        new_sheet.write(0, i + 5, '{}|{}'.format(day_of_week, day_of_month))
+
+    new_sheet.write(0, 5 + number_of_days, 'Sum')
+
+    wb.close()  # THIS writes
+
+
+def make_writer(writer, sheet_name):
+    # Get the xlsxwriter workbook and worksheet objects.
+    workbook = writer.book
+    worksheet = writer.sheets[sheet_name]
+    # Add a bold format to use to highlight cells.
+    bold = workbook.add_format({'bold': True})
+    # Widen the first column to make the text clearer.
+    worksheet.set_column('A:A', 10)
+    worksheet.set_column('B:B', 25, bold)
+    worksheet.set_column('C:E', 12)
+
+    year_month = sheet_name.split('.')
+    number_of_days = calendar.monthrange(int(year_month[0]), int(year_month[1]))[1]
+    last_column_name = 'A' + alph[(number_of_days + 5) % 27]
+    worksheet.set_column('F:{}'.format(last_column_name), 18)
+    last_column_name = 'A' + alph[(number_of_days + 5) % 27 + 1]
+    worksheet.set_column('{}:{}'.format(last_column_name, last_column_name), 25)
+    return
+
+
+def create_new_employee(filename, name):
+    """
+
+    :param name: имя сотрудника
+    :return:
+    """
+    global week, alph
+
+    data = pd.read_excel(filename, sheet_name=None)
+    sheet_name = datetime.now().strftime("%Y.%m")
+    try:
+        df = data[sheet_name]
+    except KeyError:
+        create_new_sheet(filename)
+        create_new_employee(filename, name)
+        return
+
+    # s = df[df['Фамилия, инициалы'] == name]['Фамилия, инициалы']
+    # if not s.get('Фамилия, инициалы'):
+    new_id = len(df)+1
+    val = [new_id, name, None, 'Да', datetime.now().strftime("%H:%M:%S")]
+
+    for _ in df.columns[5:-1]:
+        val.append('0ч. 0мин. 0сек.')
+
+    val.append(0)  # sum
+
+    df_add = pd.DataFrame([val], columns=df.columns)
+    df = df.append(df_add)
+
+    writer = pd.ExcelWriter(filename, engine='xlsxwriter')
+
+    for sheet, df in list(data.items())[:-1]:
+        make_writer(writer, sheet)
+        df.to_excel(writer, sheet_name=sheet, index=False)
+
+    df.to_excel(writer, sheet_name=sheet_name, index=False)
+    make_writer(writer, sheet_name)
+
+    writer.save()
+    return new_id
+
+
+def hours_minutes_seconds(seconds):
+    return seconds // 3600, (seconds // 60) % 60, seconds % 60
+
+
+def get_sum(row):
+    sheet_name = datetime.now().strftime("%Y.%m")
+    year_month = sheet_name.split('.')
+    number_of_days = calendar.monthrange(int(year_month[0]), int(year_month[1]))[1]
+    # empty result timedelta
+    td = timedelta()
+    for i in range(number_of_days):
+        day_of_month = i + 1
+        day_of_week = week[datetime.strptime('{}.{}'.format(sheet_name, day_of_month), '%Y.%m.%d').weekday()]
+        today = '{}|{}'.format(day_of_week, day_of_month)
+        h, m, s = list(map(int, re.findall(r'\d+', row[today])))
+        td += timedelta(hours=h, minutes=m, seconds=s)
+
+    d = td.days
+    h, m, s = hours_minutes_seconds(td.seconds)
+    return '{}дн. {}ч. {}мин. {}сек.'.format(d, h, m, s)
+
+
+def enter_employee(filename, id):
+    """
+
+    :param filename:
+    :param id:
+    :return:
+    """
+    data = pd.read_excel(filename, sheet_name=None)
+    sheet_name = datetime.now().strftime("%Y.%m")
+
+    try:
+        df = data[sheet_name]
+    except KeyError:
+        create_new_sheet(filename)
+        enter_employee(filename, id)
+        return
+
+    if df[df['Номер'] == id].empty:
+        return
+
+    writer = pd.ExcelWriter(filename, engine='xlsxwriter')
+
+    for sheet, _df in list(data.items())[:-1]:
+        make_writer(writer, sheet)
+        _df.to_excel(writer, sheet_name=sheet, index=False)
+
+    ser = df[df['Номер'] == id].iloc[0]
+
+    if ser['На работе'] == 'Да':
+        df.loc[id - 1, 'На работе'] = 'Нет'
+        # ser['На работе'] = 'Нет'
+        prev_time = datetime.strptime(ser['Время входа'], "%H:%M:%S")
+        # ser['Время входа'] = None
+        df.loc[id - 1, 'Время входа'] = None
+        curr_time = datetime.now().replace(microsecond=0)
+        td = curr_time - prev_time
+
+        today = '{}|{}'.format(week[datetime.now().weekday()], datetime.now().day)
+        h_old, m_old, s_old = list(map(int, re.findall(r'\d+', ser[today])))
+        td_old = timedelta(hours=h_old, minutes=m_old, seconds=s_old)
+        td_res = td + td_old
+        h_res, m_res, s_res = hours_minutes_seconds(td_res.seconds)
+        df.loc[id - 1, today] = '{}ч. {}мин. {}сек.'.format(h_res, m_res, s_res)
+        df['Суммарно за месяц'] = df.apply(get_sum, axis=1)
+
+    elif ser['На работе'] == 'Нет':
+        df.loc[id - 1, 'На работе'] = 'Да'
+        df.loc[id - 1, 'Время входа'] = datetime.now().strftime("%H:%M:%S")
+
+    df.to_excel(writer, sheet_name=sheet_name, index=False)
+    make_writer(writer, sheet_name)
+    writer.close()
+    return
+
+
+if __name__ == "__main__":
+    print('Run excel ({0})'.format(datetime.now()))
+    make_start_excel(file_name)
---
+++ server.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+"""
+asvt_fingerprint.server
+
+Very simple HTTP server in python.
+Usage::
+    ./dummy-web-server.py [<port>]
+Send a GET request::
+    curl http://localhost
+        /?name=Dima --> add new user to the table
+        /?id=1      --> enter/exit user with such id
+        /?excel=    --> get excel table in browser
+
+Send a HEAD request::
+    curl -I http://localhost
+Send a POST request::
+    curl -d "name=Dima&id=1" http://localhost
+
+Created by prudkovskiy on 29.11.18 23:50
+"""
+from datetime import datetime
+import subprocess
+from time import sleep
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse
+import cgi
+from excel_creator import create_new_employee, enter_employee, file_name
+import xlrd
+import csv
+
+__author__ = 'prudkovskiy'
+
+
+def csv_from_excel(filename):
+    csv_file_name = filename.rstrip('.xlsx') + '.csv'
+    wb = xlrd.open_workbook(file_name)
+    sheet_name = datetime.now().strftime("%Y.%m")
+    sh = wb.sheet_by_name(sheet_name)
+    with open(csv_file_name, 'w') as f:
+        wr = csv.writer(f, quoting=csv.QUOTE_ALL)
+
+        for rownum in range(sh.nrows):
+            wr.writerow(sh.row_values(rownum))
+    return csv_file_name
+
+
+class GP(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_HEAD(self):
+        self._set_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        res = urlparse(self.path).query.replace('=', ' ').replace('&', ' ').split()
+        if len(res) == 0:
+            print('stop this shit')
+            self.connection.close()
+            return
+
+        if res[0] == 'id':
+            try:
+                res[1] = int(res[1])
+
+            except Exception as ex:
+                res[1] = ord(res[1])
+                print(ex)
+
+            print(res[1])
+            enter_employee(file_name, res[1])
+            self.wfile.write(b"Success!")
+            sleep(1)
+            self.connection.close()
+
+        elif res[0] == 'name':
+            name = res[1]
+            print(name)
+            id = create_new_employee(file_name, name)
+            self.wfile.write(bytes(str(id).encode('utf-8')))
+            sleep(1.5)
+            self.connection.close()
+
+        elif res[0] == 'excel':
+            html_file = 'data.html'
+            csv_file = csv_from_excel(file_name)
+            bash_command = "excel2table {} {} -o".format(csv_file, html_file)
+            subprocess.run('bash -c "source activate root; python -V"', shell=True)
+            subprocess.call(bash_command.split())
+            # p = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE,
+            #                      stderr=subprocess.STDOUT, executable='/bin/bash')
+
+            # for line in p.stdout.readlines():
+            #     print(line.decode('utf-8'))
+            # p.wait()
+            # sleep(1)
+            with open(html_file, 'rb') as f:
+                self.wfile.write(f.read())
+            self.connection.close()
+
+        else:
+            self.connection.close()
+
+    def do_POST(self):
+        self._set_headers()
+        form = cgi.FieldStorage(
+            fp=self.rfile,
+            headers=self.headers,
+            environ={'REQUEST_METHOD': 'POST'}
+        )
+        print(form.getvalue("name"))
+        print(form.getvalue("id"))
+        self.wfile.write(b"<html><body><h1>POST Request Received!</h1></body></html>")
+
+
+def run(server_class=HTTPServer, handler_class=GP, port=8080):
+    server_address = ('192.168.1.4', port)
+    httpd = server_class(server_address, handler_class)
+    print('Server running at 192.168.1.4:8080...')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        httpd.server_close()
+
+
+if __name__ == "__main__":
+    print('Run server ({0})'.format(datetime.now()))
+    run()
